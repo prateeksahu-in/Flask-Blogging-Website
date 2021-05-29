be4f044cf1c56271ad26e49f5965f006682a1505
prateeksahu <prateeksahu0101@gmail.com>
2021-05-30 01:25:17 +0530

    updated

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from pickle import GET
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_mail import Mail
 import json
 
 with open('config.json', 'r') as c:
@@ -8,7 +11,15 @@ with open('config.json', 'r') as c:
 local_server = True
 
 app: Flask = Flask(__name__)
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
 
+)
+mail = Mail(app)
 if local_server:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
 else:
@@ -26,9 +37,23 @@ class Contacts(db.Model):
     email = db.Column(db.String(50), nullable=False)
 
 
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    slug = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
+    content = db.Column(db.String(120), nullable=False)
+
+
 @app.route("/")
 def home():
     return render_template('index.html', params=params)
+
+
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
 
 
 @app.route("/about")
@@ -46,6 +71,11 @@ def contact():
         entry = Contacts(name=name, phone_num=phone, msg=message, email=email, date=datetime.now())
         db.session.add(entry)
         db.session.commit()
+        mail.send_message('New Message From DataFit.in',
+                          sender=email,
+                          recipients=['params[gmail-user]'],
+                          body=message + "\n" + phone
+                          )
     return render_template('contact.html', params=params)
 
 
