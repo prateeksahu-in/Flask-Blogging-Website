e8a9370bd0b64cf85b2f7bf04c44b6f44be7c6d7
prateeksahu <prateeksahu0101@gmail.com>
2021-05-29 23:45:49 +0530

    updated

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from datetime import datetime
 
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app: Flask = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/datafitin'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(50), nullable=False)
 
 
 @app.route("/")
@@ -13,8 +27,16 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num=phone, msg=message, email=email, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 
