41b45f6d4e5a1f23850b82c4c7702ea39ee63cd9
prateeksahu <prateeksahu0101@gmail.com>
2021-05-30 11:17:39 +0530

    updated

--- main.py
+++ main.py
@@ -43,18 +43,20 @@ class Posts(db.Model):
     slug = db.Column(db.String(100), nullable=False)
     date = db.Column(db.String(20), nullable=True)
     content = db.Column(db.String(120), nullable=False)
+    subtitle = db.Column(db.String(120), nullable=False)
     img_file = db.Column(db.String(120), nullable=True)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html', params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params, posts=posts)
 
 
 @app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
-    post = Posts.query.filter_by(slug=post_slug).first()
-    return render_template('post.html', params=params, post=post)
+    posts = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=posts)
 
 
 @app.route("/about")
